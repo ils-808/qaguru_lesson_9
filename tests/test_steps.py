8ed945aa83369b0f6788d8896ac1be8d7fa57701
ASD <asd@asd.as>
2023-10-15 21:41:21 +0600

    pytest lib added to test_steps

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -3,6 +3,7 @@ from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
+import pytest
 
 
 @pytest.mark.skip()
